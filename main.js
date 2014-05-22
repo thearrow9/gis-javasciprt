49464686328c0c38da1e6b94775ca14d2d736859
Tihomir Yanev <tisho91@gmail.com>
2014-05-22 20:19:10 +0300

    Added overview map and test file

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
+var dojoConfig = { parseOnLoad: true };
+
 var map;
 require([
     "esri/map",
     "esri/dijit/Scalebar",
+    "esri/dijit/OverviewMap",
     "dojo/domReady!"
-    ], function(Map, Scalebar) {
+    ],
+    function(Map, Scalebar, OverviewMap) {
 
         map = new Map("mapDiv", {
             center: [0, 0],
-            zoom: 4,
+            zoom: 5,
             basemap: "streets"
         });
 
@@ -17,4 +21,15 @@ require([
             scalebarStyle: 'line',
             scalebarUnit: "metric"
         });
+
+        var overviewMapDijit = new OverviewMap({
+            map: map,
+            attachTo: "top-right",
+            visible: true,
+            color: "#D84E13",
+            height: 180,
+            width: 250,
+            opacity: .40
+        });
+        overviewMapDijit.startup();
     });
