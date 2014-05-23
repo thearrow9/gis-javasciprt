617d5d07b3b23ff8697f5a6f72c16159051aa42e
Tihomir Yanev <tisho91@gmail.com>
2014-05-24 01:04:44 +0300

    Added hide/show layer via checkbox

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 var dojoConfig = { parseOnLoad: true };
 
-var map;
+var map, dynamic_layer;
+
 require([
     'esri/map',
     'esri/dijit/Scalebar',
     'esri/dijit/Legend',
     'esri/dijit/OverviewMap',
     "esri/layers/ArcGISDynamicMapServiceLayer",
+    'dojo/_base/array',
+    'dojo/on',
+    'dojo/query',
+    'dojo/dom',
     'dojo/domReady!'
-],  function(Map, Scalebar, Legend, OverviewMap, ArcGISDynamicMapServiceLayer) {
-        map = new Map('mapDiv', {
-            center: [25, 43],
+],  function(Map, Scalebar, Legend, OverviewMap, ArcGISDynamicMapServiceLayer, arrayUtils, on, query, dom) {
+        map = new Map('map', {
+            center: [-93.5, 36.972],
             zoom: 4,
             basemap: 'streets'
         });
 
-        layer = new ArcGISDynamicMapServiceLayer(
-            "http://sampleserver6.arcgisonline.com/arcgis/rest/services/WorldTimeZones/MapServer");
+        dynamic_layer = new ArcGISDynamicMapServiceLayer("http://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer");
 
         var scalebar = new Scalebar({
             map: map,
@@ -27,22 +31,66 @@ require([
 
         var overviewMapDijit = new OverviewMap({
             map: map,
-            //attachTo: 'top-right',
             visible: true,
-            color: '#D84E13',
             height: 120,
             width: 160,
             opacity: .3
         });
 
-        var legend = new Legend({
-            map: map
-        }, "legend");
+        map.on("layers-add-result", updateLegend);
 
+        function updateLegend(evt) {
+            console.log(evt);
+            var layerInfo = arrayUtils.map(evt.layers, function (layer, index) {
+                return {layer:layer.layer, title:layer.layer.name};
+            });
+            if (layerInfo.length > 0) {
+                var legendDijit = new Legend({
+                    map: map,
+                    layerInfos: layerInfo
+                }, "legend");
+                legendDijit.startup();
+            }
+        }
 
-        map.addLayer(layer)
+        map.addLayers([dynamic_layer]);
+        dynamic_layer.on('load', buildLayerList);
+
+        function buildLayerList() {
+            var visible = [];
+            var items = arrayUtils.map(dynamic_layer.layerInfos, function(info, index) {
+                is_showed = false;
+                if (info.name == 'Counties') {
+                    visible.push(info.id);
+                    is_showed = true;
+                }
+                return '<input type="checkbox" class="list_item"' + (is_showed ? " checked" : "") +
+                    ' id="' + info.id + '" />' + '<label for="' + info.id + '">' + info.name + '</label>';
+
+            });
+            var layer_list = dom.byId("layer-list");
+            layer_list.innerHTML = items.join("\n");
+            dynamic_layer.setVisibleLayers(visible);
+            on(layer_list, "click", updateLayerVisibility);
+        }
+
+        function updateLayerVisibility() {
+            var inputs = query(".list_item");
+            var input, visible = [];
+
+            arrayUtils.forEach(inputs, function(input) {
+                if (input.checked) {
+                    visible.push(input.id);
+                }
+            });
+
+            if (visible.length === 0) {
+                visible.push(-1);
+            }
+            dynamic_layer.setVisibleLayers(visible);
+        }
+
 
         /* Init all tools */
         overviewMapDijit.startup();
-        legend.startup();
     });
