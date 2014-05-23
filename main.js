0c638f181e39e5604d1c9acfba0d1d64da835fb4
Tihomir Yanev <tisho91@gmail.com>
2014-05-23 17:22:12 +0300

    Added basic legend

--- main.js
+++ main.js
@@ -2,34 +2,47 @@ var dojoConfig = { parseOnLoad: true };
 
 var map;
 require([
-    "esri/map",
-    "esri/dijit/Scalebar",
-    "esri/dijit/OverviewMap",
-    "dojo/domReady!"
-    ],
-    function(Map, Scalebar, OverviewMap) {
-
-        map = new Map("mapDiv", {
-            center: [0, 0],
-            zoom: 5,
-            basemap: "streets"
+    'esri/map',
+    'esri/dijit/Scalebar',
+    'esri/dijit/Legend',
+    'esri/dijit/OverviewMap',
+    "esri/layers/ArcGISDynamicMapServiceLayer",
+    'dojo/domReady!'
+],  function(Map, Scalebar, Legend, OverviewMap, ArcGISDynamicMapServiceLayer) {
+        map = new Map('mapDiv', {
+            center: [25, 43],
+            zoom: 4,
+            basemap: 'streets'
         });
+
+        layer = new ArcGISDynamicMapServiceLayer(
+            "http://sampleserver6.arcgisonline.com/arcgis/rest/services/WorldTimeZones/MapServer");
 
         var scalebar = new Scalebar({
             map: map,
             attachTo: 'bottom-left',
             scalebarStyle: 'line',
-            scalebarUnit: "metric"
+            scalebarUnit: 'metric'
         });
 
         var overviewMapDijit = new OverviewMap({
             map: map,
-            attachTo: "top-right",
+            //attachTo: 'top-right',
             visible: true,
-            color: "#D84E13",
-            height: 180,
-            width: 250,
-            opacity: .40
+            color: '#D84E13',
+            height: 120,
+            width: 160,
+            opacity: .3
         });
+
+        var legend = new Legend({
+            map: map
+        }, "legend");
+
+
+        map.addLayer(layer)
+
+        /* Init all tools */
         overviewMapDijit.startup();
+        legend.startup();
     });
