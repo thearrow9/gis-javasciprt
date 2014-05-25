4acda67838e955c5c79cc27184919f7d9d3ce349
Tihomir Yanev <tisho91@gmail.com>
2014-05-25 17:27:15 +0300

    TODO feature layer update, queries

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var dojoConfig = { parseOnLoad: true };
 
-var map, draw_toolbar, edit_toolbar, draw_tool, current_color;
+var map, draw_toolbar, edit_toolbar, draw_tool, current_color, current_layer, dump = {};
 
 
 require([
@@ -13,15 +13,18 @@ require([
 
     "esri/layers/ArcGISDynamicMapServiceLayer", "esri/layers/FeatureLayer",
 
+    "dojo/parser",
     'dojo/_base/array', 'dojo/on', 'dojo/query', 'dojo/dom', 'dojo/_base/event', 'dojo/domReady!'
 ],  function(Map, Draw, Edit, Graphic, SimpleMarkerSymbol, SimpleFillSymbol, SimpleLineSymbol, Color, ColorPalette,
              Scalebar, Legend, OverviewMap, ArcGISDynamicMapServiceLayer,
-             FeatureLayer, arrayUtils, on, query, dom, event) {
+             FeatureLayer, parser, arrayUtils, on, query, dom, event) {
         map = new Map('map', {
             center: [-93.5, 36.972],
             zoom: 4,
             basemap: 'streets'
         });
+
+        parser.parse();
 
         var dynamic_layer = new ArcGISDynamicMapServiceLayer("http://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer");
 
@@ -64,7 +67,7 @@ require([
                 var legendDijit = new Legend({
                     map: map,
                     layerInfos: layerInfo
-                }, "legend");
+                }, "legend-panel");
                 legendDijit.startup();
             }
         }
@@ -77,9 +80,13 @@ require([
                     visible.push(info.id);
                     is_showed = true;
                 }
-                return '<input type="checkbox" class="list_item"' + (is_showed ? " checked" : "") +
+                var output = '';
+                output += '<input type="checkbox" class="list_item"' + (is_showed ? " checked" : "") +
                     ' id="' + info.id + '" />' + '<label for="' + info.id + '">' + info.name + '</label>';
-
+                if ((index + 1) % 2 == 0) {
+                    output += '<br />';
+                }
+                return output;
             });
             var layer_list = dom.byId("layer-list");
             layer_list.innerHTML = items.join("\n");
@@ -103,7 +110,6 @@ require([
             dynamic_layer.setVisibleLayers(visible);
         }
 
-
         function activateDrawTool() {
             draw_tool = this.value;
             query('*').style('cursor', 'crosshair');
@@ -114,13 +120,17 @@ require([
             draw_toolbar.activate(Draw[draw_tool]);
         }
 
-        function init() {
+        function init(evt) {
             createDrawTools();
             createEditTools();
             createColorPicker();
-            current_color = '#ffffff';
-        }
 
+            current_color = '#ffffff';
+            current_layer = null;
+            draw_tool = null;
+
+            printConsole();
+        }
 
         function createDrawTools() {
             draw_toolbar = new Draw(map, { showTooltips: false });
@@ -139,9 +149,8 @@ require([
             var myPalette = new ColorPalette({
                 palette: "3x4",
                 onChange: function(val){ current_color = val; }
-            }, "color-picker");
+            }, "color-panel");
         }
-
 
         function addGraphicToMap(evt) {
             var symbol;
@@ -170,7 +179,6 @@ require([
                 event.stop(evt);
                 if (draw_tool == 'DELETE') {
                     map.graphics.remove(evt.graphic);
-                    draw_toolbar.deactivate();
                     resetCursor();
                     return;
                 }
@@ -190,6 +198,13 @@ require([
                 uniformScaling: true
             };
             edit_toolbar.activate(tool, graphic, options);
+        }
+
+        function printConsole() {
+            dump.color = current_color;
+            dump.layer = current_layer;
+            dump.tool = draw_tool;
+            console.log(dump);
         }
 
         /* Init all tools */
