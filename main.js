917401eb6f8e8f1cc0aef88f86a826aa10140532
Tihomir Yanev <tisho91@gmail.com>
2014-05-27 11:42:32 +0300

    Added template picked and replaced drawing tools

--- main.js
+++ main.js
@@ -1,70 +1,178 @@
-var dojoConfig = { parseOnLoad: true };
-
-var map, draw_toolbar, edit_toolbar, draw_tool, current_color, current_layer, dump = {};
-
+var dynamicLayerUrl = 'http://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer';
+var featureServerUrl = 'http://sampleserver6.arcgisonline.com/arcgis/rest/services/Military/FeatureServer/';
 
 require([
     'esri/map',
+    "esri/dijit/BasemapToggle",
 
     "esri/toolbars/draw", "esri/toolbars/edit", "esri/graphic", "esri/symbols/SimpleMarkerSymbol",
-    "esri/symbols/SimpleFillSymbol", "esri/symbols/SimpleLineSymbol", "esri/Color", "dijit/ColorPalette",
+    "esri/symbols/SimpleFillSymbol", "esri/symbols/SimpleLineSymbol", "esri/Color",
+
+    "esri/dijit/editing/TemplatePicker",
 
     'esri/dijit/Scalebar', 'esri/dijit/Legend', 'esri/dijit/OverviewMap',
 
     "esri/layers/ArcGISDynamicMapServiceLayer", "esri/layers/FeatureLayer",
 
     "dojo/parser",
+    "dojo/_base/lang",
     'dojo/_base/array', 'dojo/on', 'dojo/query', 'dojo/dom', 'dojo/_base/event', 'dojo/domReady!'
-],  function(Map, Draw, Edit, Graphic, SimpleMarkerSymbol, SimpleFillSymbol, SimpleLineSymbol, Color, ColorPalette,
+],  function(Map, BasemapToggle, Draw, Edit, Graphic, SimpleMarkerSymbol, SimpleFillSymbol, SimpleLineSymbol, Color, TemplatePicker,
              Scalebar, Legend, OverviewMap, ArcGISDynamicMapServiceLayer,
-             FeatureLayer, parser, arrayUtils, on, query, dom, event) {
+             FeatureLayer, parser, lang, arrayUtils, on, query, dom, event) {
+
         map = new Map('map', {
             center: [-93.5, 36.972],
             zoom: 4,
             basemap: 'streets'
         });
-
         parser.parse();
 
-        var dynamic_layer = new ArcGISDynamicMapServiceLayer("http://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer");
+        var map, overviewMapDijit;
 
-        var landuseLineLayer = new FeatureLayer("http://sampleserver6.arcgisonline.com/arcgis/rest/services/Military/FeatureServer/8", {
-          mode: FeatureLayer.MODE_SNAPSHOT,
-          outFields: ["*"]
-        });
-        var landusePolygonLayer = new FeatureLayer("http://sampleserver6.arcgisonline.com/arcgis/rest/services/Military/FeatureServer/9", {
-          mode: FeatureLayer.MODE_SNAPSHOT,
-          outFields: ["*"]
-        });
+        var drawToolbar, editToolbar, selectedTemplate;
 
-        var scalebar = new Scalebar({
-            map: map,
-            attachTo: 'bottom-left',
-            scalebarStyle: 'line',
-            scalebarUnit: 'metric'
-        });
+        var legendDijit, layers, currentLayer;
 
-        var overviewMapDijit = new OverviewMap({
-            map: map,
-            visible: true,
-            height: 120,
-            width: 160,
-            opacity: .3
-        });
 
-        map.on("load", init);
-        map.on("layers-add-result", updateLegend);
-        map.on("click", function(evt) { edit_toolbar.deactivate(); });
+        var dynamicLayer = new ArcGISDynamicMapServiceLayer(dynamicLayerUrl);
+        var landusePointLayer = buildFeatureLayer(featureServerUrl + '6')
+        var landusePolylineLayer = buildFeatureLayer(featureServerUrl + '8')
 
-        map.addLayers([dynamic_layer, landuseLineLayer, landusePolygonLayer]);
-        dynamic_layer.on('load', buildLayerList);
 
-        function updateLegend(evt) {
+
+        function buildFeatureLayer(url) {
+            return new FeatureLayer(url, {
+                mode: FeatureLayer.MODE_SNAPSHOT,
+                outFields: ["*"]
+            });
+        }
+
+        function createScalebar() {
+            return new Scalebar({
+                map: map,
+                attachTo: 'bottom-left',
+                scalebarStyle: 'line',
+                scalebarUnit: 'metric'
+            });
+        }
+
+        function createOverviewMap() {
+            overviewMapDijit = new OverviewMap({
+                map: map,
+                visible: true,
+                height: 120,
+                width: 160,
+                opacity: .3
+            });
+            overviewMapDijit.startup();
+        }
+
+        function createBasemapToggle() {
+            return new BasemapToggle({
+                map: map,
+                basemap: "satellite"
+            }, "basemap-toggle").startup();
+        }
+
+        function createTemplatePicker() {
+            templatePicker = new TemplatePicker({
+                featureLayers: layers,
+                rows: "auto",
+                columns: 2,
+                grouping: true,
+                style: "height: auto; overflow: auto;"
+            }, "template-picker");
+            templatePicker.startup();
+            templatePicker.on("selection-change", changeTmpTool);
+        }
+
+        function changeTmpTool() {
+            query('*').style('cursor', 'crosshair');
+            var tmp = templatePicker.getSelected();
+            if (tmp) {
+                selectedTemplate = tmp;
+            }
+            switch (selectedTemplate.featureLayer.geometryType) {
+            case "esriGeometryPoint":
+                drawToolbar.activate(Draw.POINT);
+                break;
+            case "esriGeometryPolyline":
+                drawToolbar.activate(Draw.POLYLINE);
+                break;
+            case "esriGeometryPolygon":
+                drawToolbar.activate(Draw.POLYGON);
+                break;
+            }
+        }
+
+        map.on("load", mapInit);
+        map.on('update-end', mapUpdateEnd);
+        map.on("layers-add-result", mapLayersAdd);
+        map.on("click", function(evt) { editToolbar.deactivate(); });
+
+        dynamicLayer.on('load', createLayerList);
+
+        function mapInit(evt) {
+            map.addLayers([dynamicLayer, landusePointLayer, landusePolylineLayer]);
+            createScalebar();
+            createBasemapToggle();
+            createOverviewMap();
+        }
+
+        function mapUpdateEnd(evt) {
+            legendDijit.refresh();
+        }
+
+        function mapLayersAdd(evt) {
+            layers = arrayUtils.map(evt.layers, function(result) {
+                return result.layer;
+            });
+
+            createLegend(evt);
+            createTemplatePicker();
+            createDrawToolbar();
+            createEditToolbar();
+
+            arrayUtils.forEach(layers, function(layer) {
+                var editingEnabled = false;
+                layer.on("dbl-click", function(evt) {
+                    event.stop(evt);
+                    currentLayer = this;
+                    if (editingEnabled === false) {
+                        editingEnabled = true;
+                        activateEditToolbar(evt.graphic);
+                    } else {
+                        editToolbar.deactivate();
+                        editingEnabled = false;
+                    }
+                });
+                layer.on("click", function(evt) {
+                    event.stop(evt);
+                    if (evt.ctrlKey === true || evt.metaKey === true) {
+                        var conf = confirm('Сигурни ли сте, че искате да изтриете този обект?');
+                        if (conf) {
+                            layer.applyEdits(null,null,[evt.graphic]);
+                            currentLayer = this;
+                        }
+                        editToolbar.deactivate();
+                        editingEnabled = false;
+                    }
+                });
+            });
+        }
+
+
+
+        function createLegend(evt) {
             var layerInfo = arrayUtils.map(evt.layers, function (layer, index) {
-                return {layer:layer.layer, title:layer.layer.name};
+                if (layer.layer.supportsDynamicLayers) {
+                    return { layer:layer.layer, title:layer.layer.name };
+                }
+                else return {};
             });
             if (layerInfo.length > 0) {
-                var legendDijit = new Legend({
+                legendDijit = new Legend({
                     map: map,
                     layerInfos: layerInfo
                 }, "legend-panel");
@@ -72,9 +180,9 @@ require([
             }
         }
 
-        function buildLayerList() {
+        function createLayerList() {
             var visible = [];
-            var items = arrayUtils.map(dynamic_layer.layerInfos, function(info, index) {
+            var items = arrayUtils.map(dynamicLayer.layerInfos, function(info, index) {
                 is_showed = false;
                 if (info.name == 'Counties') {
                     visible.push(info.id);
@@ -90,7 +198,7 @@ require([
             });
             var layer_list = dom.byId("layer-list");
             layer_list.innerHTML = items.join("\n");
-            dynamic_layer.setVisibleLayers(visible);
+            dynamicLayer.setVisibleLayers(visible);
             on(layer_list, "click", updateLayerVisibility);
         }
 
@@ -107,12 +215,11 @@ require([
             if (visible.length === 0) {
                 visible.push(-1);
             }
-            dynamic_layer.setVisibleLayers(visible);
+            dynamicLayer.setVisibleLayers(visible);
         }
 
         function activateDrawTool() {
             draw_tool = this.value;
-            query('*').style('cursor', 'crosshair');
             if (draw_tool == 'DELETE') {
                 return;
             }
@@ -120,70 +227,25 @@ require([
             draw_toolbar.activate(Draw[draw_tool]);
         }
 
-        function init(evt) {
-            createDrawTools();
-            createEditTools();
-            createColorPicker();
-
-            current_color = '#ffffff';
-            current_layer = null;
-            draw_tool = null;
-
-            printConsole();
+        function createDrawToolbar() {
+            drawToolbar = new Draw(map, { showTooltips: false });
+            drawToolbar.on('draw-end', createGraphic)
         }
 
-        function createDrawTools() {
-            draw_toolbar = new Draw(map, { showTooltips: false });
-            draw_toolbar.on('draw-end', addGraphicToMap)
-            var tools = query('.tool');
-            arrayUtils.forEach(tools, function(tool) {
-                on(tool, 'click', activateDrawTool);
-            });
-        }
-
-        function createEditTools() {
-            edit_toolbar = new Edit(map);
-        }
-
-        function createColorPicker() {
-            var myPalette = new ColorPalette({
-                palette: "3x4",
-                onChange: function(val){ current_color = val; }
-            }, "color-panel");
-        }
-
-        function addGraphicToMap(evt) {
-            var symbol;
-            resetCursor();
-            draw_toolbar.deactivate();
-            switch (evt.geometry.type) {
-            case "point":
-            case "multipoint":
-                symbol = new SimpleMarkerSymbol();
-                break;
-            case "polyline":
-                symbol = new SimpleLineSymbol();
-                break;
-            default:
-                symbol = new SimpleFillSymbol();
-                break;
-            }
-            symbol.setColor(Color.fromHex(current_color));
-            var graphic = new Graphic(evt.geometry, symbol);
-            map.graphics.add(graphic);
+        function createGraphic(evt) {
+            drawToolbar.deactivate();
+            var newAttributes = lang.mixin({}, selectedTemplate.template.prototype.attributes);
+            var newGraphic = new Graphic(evt.geometry, null, newAttributes);
+            selectedTemplate.featureLayer.applyEdits([newGraphic], null, null);
+            map.graphics.add(newGraphic);
             updateEditableGraphics();
         }
 
-        function updateEditableGraphics() {
-            map.graphics.on("click", function(evt) {
-                event.stop(evt);
-                if (draw_tool == 'DELETE') {
-                    map.graphics.remove(evt.graphic);
-                    resetCursor();
-                    return;
-                }
-                activateEditToolbar(evt.graphic);
-          });
+        function createEditToolbar() {
+            editToolbar = new Edit(map);
+            editToolbar.on("deactivate", function(evt) {
+                currentLayer.applyEdits(null, [evt.graphic], null);
+            });
         }
 
         function resetCursor() {
@@ -197,16 +259,6 @@ require([
                 allowDeleteVertices: true,
                 uniformScaling: true
             };
-            edit_toolbar.activate(tool, graphic, options);
+            editToolbar.activate(tool, graphic, options);
         }
-
-        function printConsole() {
-            dump.color = current_color;
-            dump.layer = current_layer;
-            dump.tool = draw_tool;
-            console.log(dump);
-        }
-
-        /* Init all tools */
-        overviewMapDijit.startup();
     });
